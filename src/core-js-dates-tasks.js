0acbeb0dc0328242b97400e675b4623d48596006
Kate <e.khizova@gmail.com>
2024-03-21 13:41:31 +0300

    feat: add new solutions

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -93,8 +93,8 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return 32 - new Date(year, month - 1, 32).getDate();
 }
 
 /**
@@ -108,8 +108,11 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = new Date(dateStart);
+  const end = new Date(dateEnd);
+  const periodTime = end.getTime() - start.getTime();
+  return periodTime / (1000 * 3600 * 24) + 1;
 }
 
 /**
@@ -129,8 +132,11 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const dateTime = new Date(date).getTime();
+  const startTime = new Date(period.start).getTime();
+  const endTime = new Date(period.end).getTime();
+  return endTime >= dateTime && dateTime >= startTime;
 }
 
 /**
